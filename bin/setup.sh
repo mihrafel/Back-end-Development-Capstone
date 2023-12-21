a81d64afdec778039963676351a3680a243783d9
mihrafel <mihrafel@gmail.com>
2023-12-21 09:35:45 -0500

    implemented django application

--- bin/setup.sh
+++ bin/setup.sh
@@ -12,6 +12,7 @@ sudo update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.8 1
 
 echo "Checking the Python version..."
 python3 --version
+sudo apt install python3.10-venv
 
 
 echo "Creating a Python virtual environment"
